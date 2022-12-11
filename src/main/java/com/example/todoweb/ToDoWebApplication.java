e63d26123f4f6340111877638ef77a577b20c6bb
orioncsy <orioncsy@gmail.com>
2022-12-12 02:02:59 +0900

    spring boot project

---
+++ src/main/java/com/example/todoweb/ToDoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoWebApplication.class, args);
+	}
+
+}
